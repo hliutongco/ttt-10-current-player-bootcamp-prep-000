020882f1f8b8543dc7688de99889413e6d526485
Helen Liutongco <bellpickle@gmail.com>
2017-12-17 05:11:33 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |space|
-    (space == "O" || space == "X") ? counter+=1 : counter
+    (space == "O" || space == "X") ? counter+=1 : counter+=0
   end
+  counter
 end
